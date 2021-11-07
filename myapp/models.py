9a14fa7e0113a66d228a8fa0162a0597ac01b874
Soundarya <34751165+sound25@users.noreply.github.com>
2021-11-07 19:25:54 +0530

    added models and views

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class food(models.Model):
+    name=models.CharField(max_length=100)
+    carbs=models.FloatField()
+    protein=models.FloatField()
+    fats=models.FloatField()
+    calories=models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import food
 
 # Create your views here.
+def index(request):
+    Food=food.objects.all()
+    return render(request,'myapp/index.html',{'Food':Food})
