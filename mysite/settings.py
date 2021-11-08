edcf82be8d28ce7ea7445f719b9b2e91e3db2d8a
Soundarya <34751165+sound25@users.noreply.github.com>
2021-11-09 00:11:53 +0530

    Heroku Configuration

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#!_z4@zpo*y2&@(wn=+qn9v8a@3%9mpw3@*7=w+dls8vzs$in0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['','127.0.0.1']
 
 
 # Application definition
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= BASE_DIR/"staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
